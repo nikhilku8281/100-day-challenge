0f81bd95f26ce8f58740c0b401ec166a9c61bba1
nikhilku8281 <nikhil.ku.8281@gmail.com>
2023-12-10 00:10:45 -0500

    Challenge1

--- challenge1/challeng1.py
+++ challenge1/challeng1.py
@@ -1,1 +1,8 @@
-#challenge1
+#1.Create a greeting
+print("Welcome to challenge 1\n")
+#Ask the city of the user
+city=input("What is the city you grew up\n")
+#Ask the pet user likes
+pet=input("What is the pet you like\n")
+#generate the band name my adding the pet and with city
+print("Your band name is : "+city+""+pet)
