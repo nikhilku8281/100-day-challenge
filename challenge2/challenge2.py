469979ad00da10247cf8972cad5cba6ed94d4f48
nikhilku8281 <nikhil.ku.8281@gmail.com>
2023-12-10 00:59:42 -0500

    BMI problem

--- challenge2/challenge2.py
+++ challenge2/challenge2.py
@@ -4,4 +4,15 @@ height=float(input("Enter your height in meters :\n"))
 weight=float(input("Enter your weight in Kgs: \n"))
 #BMI
 BMI=round(weight/(height**2),2)
-print(f"The BMI of the person is : {BMI} \n")
+if BMI < 18.5:
+    print(f"The BMI of the person is : {BMI}, you are Underweight \n")
+elif BMI < 25:
+    print(f"The BMI of the person is : {BMI}, you are Normal \n")
+elif BMI< 30:
+    print(f"The BMI of the person is : {BMI}, you are slightly Overweight \n")
+elif BMI<35:
+    print(f"The BMI of the person is : {BMI}, you are Obese \n")
+else:
+    print(f"The BMI of the person is : {BMI}, you are clinically Obese \n")
+
+
