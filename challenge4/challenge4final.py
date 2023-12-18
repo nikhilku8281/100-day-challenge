edf1e0d4ff34edccf38db565e63002d197b7ed68
nikhilku8281 <nikhil.ku.8281@gmail.com>
2023-12-18 14:57:32 -0500

    challenge4 correct

--- challenge4/challenge4final.py
+++ challenge4/challenge4final.py
@@ -6,9 +6,13 @@ paper="Paper"
 user_choice=int(input("What do You choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
 computer_choice=random.randint(0,2)
 print(f" Computer choice {computer_choice}")
-if user_choice==2 and computer_choice==0:
-    print("\nUser Wins")
+if user_choice==0 and computer_choice==2:
+    print("\nUser choose Rock, Computer choose Scissors User Wins")
+elif user_choice==2 and computer_choice==0:
+    print("\nUser choose Scissors, Computer choose Rock, Computer Wins")
 elif user_choice < computer_choice:
     print("\nComputer wins")
+elif user_choice > computer_choice:
+    print("\nUser wins")
 else:
     print("Its a draw")
